11293226eea398bf9bc988865816aab5973423c8
stummyhurt <seantsusmilch@gmail.com>
2020-10-20 21:36:50 -0500

    Updates

---
+++ precache-manifest.6eb40b0de38c320d34b2fcd9bca54c3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348dd55c81a620891c528e3b2f36d209",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "c87c6a38ff6de3d99801",
+    "url": "/react-tutorial/static/css/main.84607452.chunk.css"
+  },
+  {
+    "revision": "aac5f617ba7478aab52a",
+    "url": "/react-tutorial/static/js/2.7de1cb97.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-tutorial/static/js/2.7de1cb97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87c6a38ff6de3d99801",
+    "url": "/react-tutorial/static/js/main.1cd9bba5.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
